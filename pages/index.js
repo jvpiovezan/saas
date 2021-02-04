527220db21df8b5d606bb7044b7bd31306de9177
vitu <jvpiovezan@hotmail.com>
2021-02-04 13:29:09 -0300

    New homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,35 @@
+import Head from 'next/head'
+import style from '../styles/Home.module.css'
+
 function Home() {
-  return <h1>Saas Home</h1>
+  return (
+    <>
+      <Head>
+        <title>Saas Home</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div
+        className={style.container}
+      >
+        <div
+          className={style.card}
+        >
+          <h1
+            className={style.title}
+          >
+            Saas Home
+          </h1>
+          <p
+            className={style.subtitle}
+          >
+            Work in Progress...
+          </p>
+        </div>
+        <div className={style.circle1}></div>
+        <div className={style.circle2}></div>
+      </div>
+    </>
+  )
 }
 
 export default Home
